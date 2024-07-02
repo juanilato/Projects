1087c7daec80d329ea5a12f35383982a25e2c0ae
Juan Ignacio Latorre <105721274+juanilato@users.noreply.github.com>
2024-07-02 00:06:25 -0300

    Add files via upload

---
+++ Python_Projects/assists database managment project/Static/script/main.js
@@ -0,0 +1,4 @@
+document.getElementById("cerrar-btn").addEventListener("click", function() {
+    var mensajeError = document.getElementById("mensaje-error");
+    mensajeError.classList.add("hide");
+});
