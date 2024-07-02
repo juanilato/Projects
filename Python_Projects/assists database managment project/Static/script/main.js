186b320d07450d00ae3e923f7e47aac77c729c94
Juan Ignacio Latorre <latorre926@gmail.com>
2024-07-02 14:49:54 -0300

    Delete Python_Projects/assists database managment project directory

--- Python_Projects/assists database managment project/Static/script/main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("cerrar-btn").addEventListener("click", function() {
-    var mensajeError = document.getElementById("mensaje-error");
-    mensajeError.classList.add("hide");
-});
